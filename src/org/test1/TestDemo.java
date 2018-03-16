e4afbdd458353e3c898c93c951b20dc334967ac0
758770768 <758770768@qq.com>
2018-03-16 22:48:32 +0800

    特

--- src/org/test1/TestDemo.java
+++ src/org/test1/TestDemo.java
@@ -3,9 +3,9 @@ package org.test1;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		System.out.println("B1");
-		System.out.println("A2");
-		System.out.println("B1");
+		System.out.println("three");
+		System.out.println("tttt");
+		System.out.println("2341");
 
 	}
 
