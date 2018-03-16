acdad0168a0363b7823a567e9d4057ffc167bdd1
758770768 <758770768@qq.com>
2018-03-16 23:09:17 +0800

    revert测试

--- src/org/test1/TestDemo.java
+++ src/org/test1/TestDemo.java
@@ -3,9 +3,12 @@ package org.test1;
 public class TestDemo {
 
 	public static void main(String[] args) {
- 
-		System.out.println("thank you very much");
- 
+
+		System.out.println("three");
+		System.out.println("tttt");
+		System.out.println("2341");
+		System.out.println("A2");
+
 	}
 
 }
