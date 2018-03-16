b626b4444f2a1a86bf4e9868f48072587b53f4f2
758770768 <758770768@qq.com>
2018-03-16 22:35:29 +0800

    he

--- src/org/test1/TestDemo.java
+++ src/org/test1/TestDemo.java
@@ -3,8 +3,8 @@ package org.test1;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		 System.out.println("A started");
-		
+		System.out.println("A started");
+		System.out.println("B start");
 
 	}
 
